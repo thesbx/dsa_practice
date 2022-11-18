3c7450a2fb5b2ccca2be55622d0c7c9490752dae
Matt Miller <mattmillerf@pm.me>
2022-11-18 12:51:56 -0500

    stuff

--- quick_sort/python/quick_sport.py
+++ quick_sort/python/quick_sport.py
@@ -8,7 +8,7 @@ def quicksort(array):
         less = [i for i in array[1:] if i <= pivot]
 
         greater = [i for i in array[1:] if i > pivot]
-
+        print(f"{less} | {pivot} | {greater}")
         return quicksort(less) + [pivot] + quicksort(greater)
 
 print (quicksort([10, 8, 1, 9, 3, 27, 46]))
