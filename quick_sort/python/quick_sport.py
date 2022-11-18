3817939891b741490df3ad1b2501c919db55b9a2
Matt Miller <mattmillerf@pm.me>
2022-11-17 23:22:49 -0500

    updates

---
+++ quick_sort/python/quick_sport.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+
+        greater = [i for i in array[1:] if i > pivot]
+
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+print (quicksort([10, 8, 1, 9, 3, 27, 46]))
