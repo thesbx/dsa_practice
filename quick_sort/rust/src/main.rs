2ab709800b8032447df33f847ac487244b025921
Matt Miller <mattmillerf@pm.me>
2023-03-29 00:34:54 -0400

    update

--- quick_sort/rust/src/main.rs
+++ quick_sort/rust/src/main.rs
@@ -1,11 +1,13 @@
-use std::time::Instant;
-fn swap(vec: &mut [u8], i: usize, j: usize) {
+use std::{time::Instant, isize};
+use sbx_common::generate_random_vec;
+
+fn swap(vec: &mut [isize], i: usize, j: usize) {
     let temp = vec[i];
     vec[i] = vec[j];
     vec[j] = temp;
 }
 
-fn partition(vec: &mut [u8], lo: usize, hi: usize) -> u8 {
+fn partition(vec: &mut [isize], lo: usize, hi: usize) -> i32 {
     let pivot = vec[hi];
     let mut index = lo;
     let mut i = lo;
@@ -18,10 +20,10 @@ fn partition(vec: &mut [u8], lo: usize, hi: usize) -> u8 {
         i += 1;
     }
     swap(vec, index, hi);
-    return index as u8;
+    return index as i32;
 }
 
-fn quicksort(vec: &mut [u8], lo: usize, hi: usize) {
+fn quicksort(vec: &mut [isize], lo: usize, hi: usize) {
     if lo >= hi {
         return;
     }
@@ -30,16 +32,8 @@ fn quicksort(vec: &mut [u8], lo: usize, hi: usize) {
     quicksort(vec, (pivot + 1) as usize, hi);
 }
 
-fn random_vec(length: usize) -> Vec<u8> {
-    let mut slice = Vec::new(); 
-    for _ in 0..length {
-        slice.push(rand::random::<u8>());
-    }
-    slice
-}
-
 fn main() {
-    let mut vec = random_vec(100);
+    let mut vec = generate_random_vec(10);
     let last_index = vec.len() - 1;
     let st = Instant::now();
     quicksort(&mut vec, 0, last_index);
